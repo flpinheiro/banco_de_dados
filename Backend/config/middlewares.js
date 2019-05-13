e51ec3cb2b3218153559a9a7165dc00e66b186f0
Pedro Oliveira <pedro.cic.unb@gmail.com>
2019-05-13 13:21:29 -0300

    Iniciando os middlewares

---
+++ Backend/config/middlewares.js
@@ -0,0 +1,7 @@
+const bodyParser = require('body-parser') /*bodyParser aplicando middleware json*/
+const cors = require('cors') /*permite o acesso da api atravez de uma outra aplicação */
+
+module.exports = app => {
+    app.use(bodyParser.json())
+    app.use(cors())
+}
